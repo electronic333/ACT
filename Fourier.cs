86df3d278723a33a72def899a11ac971ec347d75
electronic333 <42278256+electronic333@users.noreply.github.com>
2023-11-21 22:11:05 +0700

    Update Fourier.cs

--- Fourier.cs
+++ Fourier.cs
@@ -38,20 +38,4 @@ public static class Fourier {
     
     return result;
   }
-
-  public static Complex[] LinearConvolution (Complex[] input1, Complex[] input2) {
-    var result = new Complex[input1.Length + input2.Length - 1];
-
-    for (int i = 0; i < input1.Length; i++) {
-      for (int j = 0; j < input2.Length; j++) {
-        result[i + j] = input1[i] * input2[j];
-      }
-    }
-
-    return result;
-  }
-
-  public static Complex[] FourierConvolution (float[] a, float[] b, Func<float[], Complex[]> straight, Func<Complex[], Complex[]> reversed) {
-    return reversed(Enumerable.Zip(straight(a), straight(b), (x, y) => x * y).ToArray());
-  }
 }
